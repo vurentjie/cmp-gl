c8c558e0715a8b9dc8ba3e7370d41e2d11a2f286
vurentjie <vurentjie@gmail.com>
2024-04-21 20:34:27 +0200

    main: test keyword pattern

--- lua/cmp_gl/init.lua
+++ lua/cmp_gl/init.lua
@@ -4,6 +4,10 @@ function source.new()
   return setmetatable({}, { __index = source })
 end
 
+function source:get_keyword_pattern()
+    return '\\<gl\\|\\<GL_*'
+end
+
 function source:complete(_, callback)
   local ok, fn = pcall(require, string.format('cmp_gl.filetypes.%s', vim.bo.filetype))
   if ok then
